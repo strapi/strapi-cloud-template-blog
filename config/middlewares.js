61b3cc4db9babd526aa6b9d0ae45fb8acbe061b2
James Neema <boosticalimited@gmail.com>
2026-08-18 01:16:11 +0300

    Refactor middlewares configuration to use ES6 export

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,38 @@
-module.exports = [
-  'strapi::logger',
+export default [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'market-assets.strapi.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://bidiibrands.com',
+        'https://www.bidiibrands.com',
+        'https://bdii.co.ke',
+        'https://www.bdii.co.ke',
+        'https://bd-ii.com',
+        'https://www.bd-ii.com',
+        'http://localhost:8080',
+        'http://localhost:5173',
+        'http://localhost:3000',
+      ],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
